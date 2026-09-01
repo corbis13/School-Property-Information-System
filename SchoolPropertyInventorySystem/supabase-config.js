63267c80d5ae8c3c96b53a119e49eaf36228a699
corbis13 <corbis.official@gmail.com>
2026-09-01 15:45:27 +0800

    Merge branch

--- SchoolPropertyInventorySystem/supabase-config.js
+++ SchoolPropertyInventorySystem/supabase-config.js
@@ -1,5 +1,5 @@
 window.SUPABASE_CONFIG = {
   url: "https://ouqgkytallctnptshefo.supabase.co",
-  anonKey: "sb_publishable_pQyUd8E-iLQzPEFTkWLmEg_nlTh3ba-",
+  anonKey: "sb_publishable_UDhp6lrRgVppuqH6Uu4Izg_zp7T-_WS",
   assetUrl: "https://corbis13.github.io/School-Property-Information-System/SchoolPropertyInventorySystem/asset.html"
 };
